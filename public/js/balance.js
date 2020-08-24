672e0af044d50ee52fc972362c22d5ac898542a8
jedrzejgraczykowski <jedrzej.graczykowski@htdevelopers.com>
2020-08-25 01:26:16 +0200

    add balance view

---
+++ public/js/balance.js
@@ -0,0 +1,97 @@
+$(document).ready(function () {
+  var incomes = $("#incomes li");
+  var expenses = $("#expenses li");
+  var balanceWindow = $("#balanceWindow");
+  var balance = 0;
+  for (let i = 0; i < incomes.length; i++) {
+    balance += Number(incomes[i].innerHTML);
+  }
+  for (let i = 0; i < expenses.length; i++) {
+    balance -= Number(expenses[i].innerHTML);
+  }
+  if (balance >= 0) {
+    balanceWindow.css("background-color", "green");
+    balanceWindow.html(
+      "Your balance is: " + balance.toFixed(2) + "<br/>Well done! You've saved some money!"
+    );
+  } else {
+    balanceWindow.css("background-color", "red");
+    balanceWindow.html(
+      "Your balance is: " + balance.toFixed(2) + "<br/>Watch Out! You're going into debts!"
+    );
+  }
+});
+
+//Finance chart - Incomes
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var incomesItems = $("#incomesItems li");
+  var expensesItems = $("#expensesItems li");
+  var incomes = $("#incomes li");
+  var expenses = $("#expenses li");
+
+  var itemsIncomesArray = [["Income Item", "Amount"]];
+  for (let i = 0; i < incomesItems.length; i++) {
+    if (Number(incomes[i].innerHTML) > 0)
+      itemsIncomesArray.push([
+        incomesItems[i].innerHTML,
+        Number(incomes[i].innerHTML),
+      ]);
+  }
+
+  var itemsExpensesArray = [["Expense Item", "Amount"]];
+  for (let i = 0; i < expensesItems.length; i++) {
+    if (Number(expenses[i].innerHTML) > 0)
+      itemsExpensesArray.push([
+        expensesItems[i].innerHTML,
+        Number(expenses[i].innerHTML),
+      ]);
+  }
+
+  var incomesData = google.visualization.arrayToDataTable(itemsIncomesArray);
+  var expensesData = google.visualization.arrayToDataTable(itemsExpensesArray);
+
+  var incomesOptions = {
+    title: "Incomes",
+    is3D: true,
+    backgroundColor: "#696969",
+    chartArea: { left: 40, top: 50, width: "100%", height: "90%" },
+    legend: {
+      position: "right",
+      alignment: "center",
+      textStyle: { color: "#fefefe", fontSize: 16 },
+    },
+    titleTextStyle: { color: "#fefefe", fontSize: 20 },
+  };
+
+  var expensesOptions = {
+    title: "Expenses",
+    is3D: true,
+    backgroundColor: "#696969",
+    chartArea: { left: 40, top: 50, width: "100%", height: "90%" },
+    legend: {
+      position: "right",
+      alignment: "center",
+      textStyle: { color: "#fefefe", fontSize: 16 },
+      pagingTextStyle: { color: "#fefefe" },
+      scrollArrows: {
+        activeColor: "#fefefe",
+        inactiveColor: "#979797",
+      },
+    },
+    titleTextStyle: { color: "#fefefe", fontSize: 20 },
+  };
+
+  var incomesChart = new google.visualization.PieChart(
+    document.getElementById("piechart_3d_incomes")
+  );
+
+  var expensesChart = new google.visualization.PieChart(
+    document.getElementById("piechart_3d_expenses")
+  );
+
+  incomesChart.draw(incomesData, incomesOptions);
+  expensesChart.draw(expensesData, expensesOptions);
+}
