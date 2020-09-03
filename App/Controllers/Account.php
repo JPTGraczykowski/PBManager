db50d629d08a38a3f900bac88c3037f99d080365
jedrzejgraczykowski <jedrzej.graczykowski@htdevelopers.com>
2020-09-03 15:27:00 +0200

    move updating methods to Account controller

--- App/Controllers/Account.php
+++ App/Controllers/Account.php
@@ -4,9 +4,49 @@ namespace App\Controllers;
 
 use \App\Models\User;
 use \App\Auth;
+use \App\Flash;
 
-class Account extends \Core\Controller
+class Account extends Authenticated
 {
+    protected function before()
+    {
+        parent::before();
+        $this->user = Auth::getUser();
+    }
+
+
+    public function updateNameAction()
+    {
+        if ($this->user->updateName($_POST['name'])) {
+        Flash::addMessage('Name changed successfully');
+        $this->redirect('/Settings/show');
+        } else {
+        Flash::addMessage('Unsuccessful changing name', Flash::WARNING);
+        }
+    }
+
+
+    public function updateEmailAction()
+    {
+        if ($this->user->updateEmail($_POST['email'])) {
+        Flash::addMessage('Email changed successfully');
+        $this->redirect('/Settings/show');
+        }  else {
+        Flash::addMessage('Unsuccessful changing email', Flash::WARNING);
+        }
+    }
+
+
+    public function updatePasswordAction()
+    {
+        if ($this->user->updatePassword($_POST['old_password'], $_POST['new_password'], $_POST['password_confirmation'])) {
+        Flash::addMessage('Password changed successfully');
+        $this->redirect('/Settings/show');
+        } else {
+        Flash::addMessage('Unsuccessful changing password', Flash::WARNING);
+        }
+    }
+
     public function validateEmailAction()
     {
         $is_valid = ! User::emailExists($_GET['email'], $_GET['ignore_id'] ?? null);
--- App/Controllers/Settings.php
+++ App/Controllers/Settings.php
@@ -4,11 +4,9 @@ namespace App\Controllers;
 
 use \Core\View;
 use \App\Auth;
-use \App\Flash;
 use \App\Models\ExpenseCategoriesAssignedToUser;
 use \App\Models\IncomeCategoriesAssignedToUser;
 use \App\Models\PaymentMethodsAssignedToUser;
-use \App\Models\User;
 
 class Settings extends Authenticated
 {
@@ -35,35 +33,5 @@ class Settings extends Authenticated
         'user' => $this->user
       ]
     );
-  }
-
-  public function updateNameAction()
-  {
-    if ($this->user->updateName($_POST['name'])) {
-      Flash::addMessage('Name changed successfully');
-      $this->redirect('/Settings/show');
-    } else {
-      Flash::addMessage('Unsuccessful changing name', Flash::WARNING);
-    }
-  }
-
-  public function updateEmailAction()
-  {
-    if ($this->user->updateEmail($_POST['email'])) {
-      Flash::addMessage('Email changed successfully');
-      $this->redirect('/Settings/show');
-    }  else {
-      Flash::addMessage('Unsuccessful changing email', Flash::WARNING);
-    }
-  }
-
-  public function updatePasswordAction()
-  {
-    if ($this->user->updatePassword($_POST['old_password'], $_POST['new_password'], $_POST['password_confirmation'])) {
-      Flash::addMessage('Password changed successfully');
-      $this->redirect('/Settings/show');
-    } else {
-      Flash::addMessage('Unsuccessful changing password', Flash::WARNING);
-    }
   }
 }
